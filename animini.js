c52e1ae3d9e83c222b637d9bc6dc127167d9061e
Roy Sharon <roy@roysharon.com>
2012-01-23 13:37:35 +0200

    Revised animation creation to a much simpler way. Added tests.

--- animini.js
+++ animini.js
@@ -1,43 +1,106 @@
-// animini.js - Miniature Animation Javascript Library
+// animini.js - Animation Micro Javascript Library
 // by Roy Sharon <roy@roysharon.com>, 2010-09-27
 // Using or modifying this project is subject to the very permissive MIT License (http://creativecommons.org/licenses/MIT/)
 
 (function () {
 
+
 	//----- Utilities --------------------------------------------------------------
 	
-	function expose(path, o) { 
-		for (var i = 0, c = ('animini.'+path).split('.'), n = c.length, t = window; i < n; ++i)
-			if (t[c[i]] == undefined) t = t[c[i]] = i == n-1 ? o : {}; 
-			else t = t[c[i]];
-	}
-	
-	function pre(s, n, p) {
-		if (!n) return s;
-		
-		if (p == undefined || p == '') p = '0';
-		while (p.length < n) p += p;
-		
-		var r = p + s;
-		return r.substr(r.length - n);
-	}
-	
 	function toCamelCase(s) {
-		return s.toLowerCase().replace(/-([a-z])/g, function (ig, m) { return m.toUpperCase(); });
+		return s.replace(/-([a-z])/g, function (ig, m) { return m.toUpperCase(); });
 	}
 	
+	function numToString(v) {
+		return '' + v;
+	}
 	
+	function colorComponentToString(v) {
+		var s = '0' + Math.round(v).toString(16);
+		return s.substr(s.length - 2);
+	}
+	
+
+	//----- Parsing CSS Props -----------------------------------------------------
+		
+	function appendVal(s, v) { return v ? (s ? s + ' ' : '') + v : s; }
+
+	var propTrimRE = /^(?:\s*;)*\s*|\s*(?:;\s*)*$/g;
+	var propSplitRE = /(?:\s*;)+\s*/g;
+	var propValRE = /\s*:\s*/;
+	var valRE = /^(.*?)(-?(?:\d+(?:\.\d*)?|\.\d+))(.*)$/;
+	var colorRE = /#(?:[0-9a-f]{6}|[0-9a-f]{3})\b/ig;
+	var shortColorRE = /#([0-9a-f])([0-9a-f])([0-9a-f])\b/g;
+	var whiteSpace = /\s+/g;
+	
+	function toLower(s) { return s.toLowerCase(); }
+	
+	function parseCssText(s) {
+		for (var r = {}, a = (s || '').replace(propTrimRE, '').split(propSplitRE), i = a.length - 1; i >= 0; --i) {
+			var p = a[i].split(propValRE, 2);
+			if (p.length == 2 && p[1]) r[toCamelCase(p[0])] = p[1].replace(colorRE, toLower).replace(shortColorRE, '#$1$1$2$2$3$3');
+		}
+		return r;
+	}
+	
+	function parseAnimatedVals(fa, ta) {
+		var vals = [], last;
+		
+		function addVal(prefix, postfix, start, end, to) {
+			vals.push(last = {start:start, delta:end - start, to:to});
+			if (prefix) last.prefix = prefix;
+			if (postfix) last.postfix = postfix;
+		} 
+		
+		for (var i = 0, n = fa.length, b = 0; i < n; ++i) {
+			var fi = fa[i], ti = ta[i];
+			if (fi == ti) continue;
+			var prefix = fa.slice(b, i > 0 ? i : 0).join(' '), postfix = '';
+			if (/^#/.test(fi)) {
+				prefix = appendVal(prefix, '#');
+				
+				function compareColorComponent(k) {
+					var fk = fi.substr(k, 2), tk = ti.substr(k, 2);
+					if (fk != tk) { 
+						addVal(prefix, postfix, parseInt(fk, 16), parseInt(tk, 16), colorComponentToString);
+						prefix = postfix = '';
+					} else prefix += fk;
+				}
+				compareColorComponent(1);
+				compareColorComponent(3);
+				compareColorComponent(5);
+				
+				if (prefix) last.postfix = prefix;
+			} else {
+				var fm = valRE.exec(fi), tm = valRE.exec(ti);
+				if (!fm || !tm) continue;
+
+				var fk = parseFloat(fm[2]), tk = parseFloat(tm[2]);
+				if (fk == tk) continue;
+				
+				addVal(appendVal(prefix, fm[1]), fm[3], fk, tk, numToString);
+			}
+			b = i + 1;
+		}
+		if (!last) return fa.join(' ');
+		last.postfix = appendVal(last.postfix, fa.slice(b).join(' '));
+		return vals;
+	}
+	
+		
 	//----- Easing functions -------------------------------------------------------
 
-	function out(f) { return function (pos) { return 1 - f(1 - pos); }; }
+	function markAsEasing(f, n, t) { f.isEasing = n + '.' + t; return f; }
 
-	function inout(f) { return function (pos) { return pos < 0.5 ? f(pos * 2) / 2 : (1 - f(2 - pos * 2) / 2); }; }
+	function out(f, n) { return markAsEasing(function (pos) { return 1 - f(1 - pos); }, n, 'out'); }
 
-	function easing(easeInFunc) {
-		var f = inout(easeInFunc);
-		f['in'] = easeInFunc;
-		f['out'] = out(easeInFunc);
-		f['inout'] = inout(easeInFunc);
+	function inout(f, n) { return markAsEasing(function (pos) { return pos < 0.5 ? f(pos * 2) / 2 : (1 - f(2 - pos * 2) / 2); }, n, 'inout'); }
+
+	function easing(easeInFunc, name) {
+		var f = inout(easeInFunc, name);
+		f['in'] = markAsEasing(easeInFunc, name, 'in');
+		f['out'] = out(easeInFunc, name);
+		f['inout'] = inout(easeInFunc, name);
 		return f;
 	}
 	
@@ -62,46 +125,215 @@
 		}
 	};
 	
-	function setupEasingFuncs() {
-		for (var i in easingFuncs) easing[i] = easing(easingFuncs[i]);
+	function setupEasingFuncs(o) {
+		for (var i in easingFuncs) o[i] = easing(easingFuncs[i], i);
 	}
-	setupEasingFuncs();
-	
-	expose('easing', easing);
-	
-	
+		
+		
 	//----- Animation class ---------------------------------------
 	
-	function Animation() {}
-	
-	expose('Animation', Animation);
+	function Animation() {
+		this.args = [];
+		this.elements = [];
+	}
 	
 	Animation.prototype = {
-	
-		millisec : 1000,
 		
-		easingFunc : easing['sine']['inout'],
+		clone :
+		function () {
+			var r = new Animation();
+			r.millisec = this.millisec;
+			r.elements = this.elements.concat();
+			r.args = this.args.concat();
+			return r;
+		},
+		
+		
+		//----- parsing -----
+		
+		parse :
+		function () {
+			this.parseArg(Array.prototype.slice.call(arguments, 0));
+			//this.parseNonArrayArg(' ');
+			delete this.notstart;
+			if (this.compile() && this.elements.length) this.start();
+		},
+		
+		parseArg :
+		function (arg) {
+			if (arg instanceof Array) for (var i = 0, n = arg.length; i < n; ++i) this.parseArg(arg[i]);
+			else this.parseNonArrayArg(arg);
+		},
+		
+		parseNonArrayArg :
+		function (arg) {
+			switch (typeof(arg)) {
+				case 'string':
+					if (arg) {
+						var o = document.getElementById(arg);
+						if (o && typeof(o.style) == 'object') this.elements.push(o);
+						else { this.args.push({p:'s', v:arg}); this.notstart = true; }
+					}
+					break
+				
+				case 'object':
+					if (typeof(arg.style) == 'object') this.elements.push(arg);
+					break;
+					
+				case 'number':
+					if (!this.notstart) { this.millisec = arg; this.notstart = true; }
+					else this.args.push({p:'m', v:arg});
+					break;
+				
+				case 'function':
+					this.args.push({p:arg.isEasing ? 'e' : 'c', v:arg});
+					this.notstart = true;
+					break;
+			}
+		},
+		
+		compile :
+		function () {
+			var props = {}, times = [], efuncs = [], callbacks = [], count = 0, millis = null, efunc = null, cb = [], dup;
+			
+			function addStep(p) {
+				times.push(millis); millis = null;
+				efuncs.push(efunc || defaultEasing); efunc = null;
+				callbacks.push(cb.length ? cb : undefined); cb = [];
+				if (!p) p = {__:'' + count};
+				else if (dup == undefined) dup = p;
+				else if (dup) dup = false;
+				for (var k in props) if (p[k]) {
+					var a = props[k], l = a[a.length - 1], v = p[k].split(whiteSpace), t = v.join(' ');
+					if (l.v.length != v.length) l.z = true;
+					props[k].push({v:v, t:t, i:count});
+					delete p[k];
+				}
+				for (k in p) {
+					var v = p[k].split(whiteSpace);
+					props[k] = [{v:v, t:v.join(' '), i:count}];
+				}
+				++count;
+			}
+			
+			for (var i = 0, n = this.args.length; i < n; ++i) {
+				var arg = this.args[i], v = arg.v;
+				switch (arg.p) {
+					case 's': 
+						addStep(parseCssText(arg.v));
+						break;
+						
+					case 'e':
+						efunc = v;
+						break;
+					
+					case 'm':
+						if (count < 0) {
+							millis = v;
+							addStep();
+						}
+						if (millis) {
+							addStep();
+							millis = v;
+						} else millis = v;
+						break;
+					
+					case 'c':
+						cb.push(v);
+						if (!millis) addStep();
+						break;
+				}
+			}
+			if (millis || cb.length) addStep(dup);
+			else if (dup) addStep(dup);
+			
+			if (!count) return;
+			
+			// calculate the duration of each stage
+			for (var i = 0, m = 0, z = 0, k = 1; i < count; ++i) {
+				if (times[i] != undefined) m += times[i];
+				else if (i > 0) ++z;
+			}
+			if (z) var k = 1, d = this.millisec ? Math.max(0, Math.round((this.millisec - m) / z)) : 1000;
+			else k = this.millisec ? this.millisec / m : 1, d = this.millisec || 1000;
+			for (var i = 0, starts = [0]; i < count; ++i) {
+				times[i] = times[i] == undefined ? i ? d : 0 : Math.round(k * times[i]);
+				starts.push(starts[i] + times[i]);
+			}
+			
+			// build stages
+			var stages = [];
+			for (k in props) {
+				var a = props[k], i = a.length - 1;
+				if (i && k != '__') {
+					while (i) {
+						var before = a[i-1], after = a[i--], bi = before.i, ai = after.i;
+						for (var j = bi + 1, m = 0; j <= ai; ++j) m += times[j];
+						var vals = before.z ? after.t : parseAnimatedVals(before.v, after.v);
+						var e = vals instanceof Array ? efuncs[ai] : undefined;
+						stages.push({t:starts[bi+1], d:m, c:callbacks[ai], e:e, p:k, v:vals});
+					}
+				} else {
+					while (i >= 0) {
+						var state = a[i--], si = state.i, w = k == '__', s = {t:starts[si+1], d:0, c:callbacks[si]};
+						if (!w) { s.p = k; s.v = state.t; }
+						stages.push(s);
+					}
+				}
+			}
+			stages.sort(function (a, b) {
+				var at = a.t, bt = b.t, ad = a.d, bd = b.d;
+				return at > bt ? 1 : at < bt ? -1 : ad > bd ? 1 : ad < bd ? -1 : 0;
+			});
+			
+			this.stages = stages;
+			return true;
+		},
+		
+		
+		//----- actual animation -----
 		
 		start :
-		function start() {
-			this.frame = 0;
-			this.pos = 1 / this.millisec;
-			this.startTime = new Date().getTime();
-			this.endTime = this.startTime + this.millisec;
-			this.setTimer(this.step, 1);
-			this.doStep();
+		function () {
+			this.stage = -1;
+			this.nextStage();
+		},
+		
+		nextStage :
+		function (fastforward) {
+			if (this.stage < this.stages.length - 1) {
+				var s = this.stages[++this.stage];
+				this.callbacks = s.c;
+				if (s.p) {
+					this.props = s.p;
+					if (!fastforward) {
+						this.frame = 0;
+						this.pos = 1 / s.m;
+						this.easingFunc = s.e;
+						this.startTime = new Date().getTime();
+						this.endTime = this.startTime + s.m;
+						this.setTimer(this.step, 1);
+					} else this.pos = 1;
+					this.doStep();
+				} else if (!fastforward) this.setTimer(this.dispatchCallbacks);
+			}
+		},
+		
+		dispatchCallbacks :
+		function () {
+			for (var i = 0, n = this.callbacks.length; i < n; ++i) this.callbacks[i](this.elements);
+			this.nextStage();
 		},
 		
 		stop :
-		function stop() {
+		function () {
 			if (this.timeout != undefined) clearTimeout(this.timeout);
 			delete this.timeout;
-			this.pos = 1;
-			this.doStep();
+			while (this.stage < this.stages.length) this.nextStage(true);
 		},
 		
 		step :
-		function step() {
+		function () {
 			var t = new Date().getTime(), d = this.endTime - t;
 			if (d > 0) {
 				this.fpms = ++this.frame / (t - this.startTime);
@@ -110,215 +342,50 @@
 			} else this.pos = 1;
 			this.doStep();
 			if (this.pos < 1) this.setTimer(this.step, 10);
-			else { delete this.timeout; if (this.onend) this.onend(); }
+			else { delete this.timeout; this.dispatchCallbacks(); }
 		},
 		
 		doStep :
-		function doStep() {},
-		
-		setTimer :
-		function setTimer(f, ms) {
-			this.timeout = window.setTimeout(this.cb(f), ms);
+		function () {
+			var y = this.easingFunc(this.pos), o = this.elements;
+			for (var p = this.props, i = p.length - 1; i >= 0; --i) {
+				for (var j = 0, s = [], a = p[i].vals, n = a.length; j < n; ++j) {
+					var c = a[j];
+					s.push(c.prefix + c.to(y * c.delta + c.start) + c.postfix);
+				}
+				for (j = o.length - 1; j >= 0; --j) o[j].style[p[i].prop] = s.join('');
+			}
 		},
 		
-		cb :
-		function cb(func) {
+		setTimer :
+		function (f, ms) {
 			var me = this;
-			return function() { return func.apply(me, arguments); };
+			this.timeout = window.setTimeout(function () { f.call(me); }, ms);
 		}
 	
 	};
-		
-	
-	//----- Parallel class ---------------------------------------------------------
-	
-	function Parallel(millisec, onend) {
-		this.animations = [];
-		if (millisec) this.millisec = millisec;
-		this.onend = onend;
-	}
-	
-	expose('Parallel', Parallel);
-	
-	Parallel.prototype = new Animation();
-	
-	Parallel.prototype.add =
-	function (animation) {
-		this.animations.push(animation);
-	};
-	
-	Parallel.prototype.start =
-	function () {
-		for (var i = 0, n = this.animations.length; i < n; ++i) {
-			var a = this.animations[i];
-			a.setTimer = function(){};
-			a.millisec = this.millisec;
-			a.start();
-		}
-		Animation.prototype.start.call(this);
-	};
-	
-	Parallel.prototype.stop =
-	function () {
-		for (var i = 0, n = this.animations.length; i < n; ++i) this.animations[i].stop();
-	};
-	
-	Parallel.prototype.doStep =
-	function () {
-		for (var i = 0, n = this.animations.length; i < n; ++i) this.animations[i].step();
-	};
-	
-	
-	//----- Serial class -----------------------------------------------------------
-	
-	function Serial(onend) {
-		this.animations = [];
-		this.onend = onend;
-		this.current = -1;
-	}
-	
-	expose('Serial', Serial);
-	
-	Serial.prototype = new Animation();
-	
-	Serial.prototype['add'] =
-	function (animation) {
-		this.animations.push(animation);
-	};
-	
-	Serial.prototype.start =
-	function () {
-		for (var i = 0, n = this.animations.length; i < n; ++i) this.animations[i].onend = this.cb(this.next);
-		this.current = -1;
-		this.next();
-	};
-	
-	Serial.prototype.stop =
-	function () {
-		if (this.current != -1) this.animations[this.current].stop();;
-		this.current = -1;
-	};
-	
-	Serial.prototype.doStep =
-	function () {
-		if (this.current != -1) this.animations[this.current].doStep();;
-	};
-	
-	Serial.prototype.next =
-	function () {
-		if (++this.current < this.animations.length) this.animations[this.current].start();
-		else { this.current = -1; if (this.onend) this.onend(); }
-	};
-	
-	
-	//----- Pause class ------------------------------------------------------------
-	
-	function Pause(millisec, onend) {
-		this.millisec = millisec;
-		this.onend = onend;
-	}
-	
-	expose('Pause', Pause);
-	
-	Pause.prototype = new Animation();
-	
+
 	
 	//----- Animation Factory -----------------------------------------------------
 	
-	function Custom(o, props, millisec, easingFunc, onend) {
-		this.obj = o;
-		this.props = props;
-		if (millisec) this.millisec = millisec;
-		this.onend = onend;
-		if (easingFunc) this.easingFunc = easingFunc;
+	function create() {
+		return parseFunc().apply(null, arguments);
 	}
 	
-	Custom.prototype = new Animation();
+	function parseFunc(animation) {
+		return function () {
+			var a = animation ? animation.clone() : new Animation();
+			a.parse.apply(a, arguments);
+			var r = parseFunc(a);
+			r.animation = a;
+			return r;
+		};
+	}
 	
-	Custom.prototype.doStep = 
-	function () {
-		var y = this.easingFunc(this.pos);
-		for (var p = this.props, i = p.length - 1; i >= 0; --i) {
-			for (var j = 0, s = [], a = p[i].vals, n = a.length; j < n; ++j) {
-				var c = a[j];
-				s.push(c.prefix + c.to(y * c.delta + c.start) + c.postfix);
-			}
-			this.obj.style[p[i].prop] = s.join('');
-		}
-	};
-	
-	var valRE = /^(.*?)(-?(?:\d+(?:\.\d*)?|\.\d+))(.*)$/;
-	var colorRE = /^#([0-9a-f])([0-9a-f])([0-9a-f])$/;
-	
-	function create(o, fromCssText, toCssText, millisec, easingFunc, onend) {
-		var from = parseCssText(fromCssText), to = parseCssText(toCssText), r = [];
-		for (var p in from) {
-			var tp = to[p];
-			if (!tp) continue;
-			var fa = from[p].split(/\s+/g), n = fa.length, ta = tp.split(/\s+/g);
-			if (ta.length != n) continue;
-			for (var i = 0, b = 0, d = [], last; i < n; ++i) {
-				var fi = fa[i], ti = ta[i];
-				if (fi == ti) continue;
-				var c = {prefix:fa.slice(b, i > 0 ? i : 0).join(' '), postfix:''};
-				if (/^#/.test(fi)) {  // possibly a color in 3 hex digits
-					fi = fi.toLowerCase().replace(colorRE, '#$1$1$2$2$3$3');
-					ti = ti.toLowerCase().replace(colorRE, '#$1$1$2$2$3$3');
-					if (fi == ti) continue;
-					
-					var addToPrefix = '';
-					c.prefix = appendVal(c.prefix, '#');
-					function compareColorComponent(k) {
-						var fk = fi.substr(k, 2), tk = ti.substr(k, 2);
-						if (fk != tk) { 
-							c.start = parseInt(fk, 16);
-							c.delta = parseInt(tk, 16) - c.start;
-							c.to = colorComponentTo;
-							d.push(last = c);
-							c = {prefix:'', postfix:''};
-						} else c.prefix += fk;
-					}
-					compareColorComponent(1);
-					compareColorComponent(3);
-					compareColorComponent(5);
-					if (c.prefix) d[d.length - 1].postfix = c.prefix;
-				} else {
-					var fm = valRE.exec(fi), tm = valRE.exec(ti);
-					if (!fm || !tm) continue;
+	create['easing'] = easing;
+	setupEasingFuncs(create);
+	var defaultEasing = create['sine']['inout'];
 
-					addToPrefix = fm[1];
-					var fk = parseFloat(fm[2]), tk = parseFloat(tm[2]);
-					if (fk == tk) continue;
-					
-					c.start = fk;
-					c.delta = tk - fk;
-					c.to = numTo;
-					c.postfix = fm[3];
-					d.push(last = c);
-				}
-				last.prefix = appendVal(last.prefix, addToPrefix);
-				b = i + 1;
-			}
-			last.postfix = appendVal(last.postfix, fa.slice(b).join(' '));
-			r.push({prop:p, vals:d});
-		}
-		return new Custom(o, r, millisec, easingFunc, onend);
-	}
-	
-	function appendVal(s, v) { return (s ? s + ' ' : '') + v; }
-
-	function numTo(v) { return '' + v; }
-	
-	function colorComponentTo(v) { var s = '0' + Math.round(v).toString(16); return s.substr(s.length - 2); }
-	
-	function parseCssText(s) {
-		for (var r = {}, a = (s || '').replace(/^\s+|\s*;\s*$/g, '').split(/\s*;\s*/g), i = a.length - 1; i >= 0; --i) {
-			var p = a[i].split(/\s*:\s*/, 2);
-			if (p.length == 2 && p[1]) r[toCamelCase(p[0])] = p[1];
-		}
-		return r;
-	}
-	
-	expose('create', create);
+	window['animini'] = create;
 
 })();
